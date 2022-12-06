d2fd170b6545c6a84a9418e443e3ec66e305a220
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2022-12-06 19:09:40 +0300

    add middlewares for logging

--- internal/routes.go
+++ internal/routes.go
@@ -1,10 +1,21 @@
 package internal
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func (s *server) routes() {
+	s.mux.Use(middleware.RequestID)
+	s.mux.Use(middleware.RealIP)
+	s.mux.Use(middleware.Logger)
+	s.mux.Use(middleware.Recoverer)
+
 	// this prefix is always required.
 	s.mux.Route("/api/{university}", func(r chi.Router) {
+		// trailing slashes are ignored in this subrouter.
+		r.Use(middleware.StripSlashes)
+
 		r.Get("/timestamp/{ts:[0-9]+}", s.handleTimestamp())
 		r.Get("/date/{date}", s.handleDate())
 		r.Get("/today", s.handleToday())
